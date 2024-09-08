2ae8c4af526f59a9e7813cbe81dda49fb8eabfb0
Marc Lavergne <marc@inferise.com>
2024-09-07 23:31:51 -0400

    Add icons

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Inferise
 
-Public Web
+Public Resources
--- index.js
+++ index.js
@@ -2,8 +2,12 @@ import { createApp, reactive } from "https://unpkg.com/petite-vue?module";
 
 var links = [
     {
-        url: "https://inferise.github.io/public/",
-        label: "Bug Report"
+        url: "https://github.com/inferise/public/issues/new?assignees=&labels=&projects=&template=bug_report.md&title=",
+        label: "File A Bug Report"
+    },
+    {
+        url: "https://github.com/inferise/public/issues/new?assignees=&labels=&projects=&template=feature_request.md&title=",
+        label: "File A Feature Request"
     },
     {
         url: "https://apps.apple.com/us/app/inferisehub/id1111111111",
